b95e9a81801e54bbd4c2db0d89483b56ccbe0047
Tatsuya Ichikawa <>
2018-12-17 21:11:47 +0900

    [Add] Add vuex stationery

---
+++ src/store/actions.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+/* eslint-disable no-unused-vars */
+
+export default {
+  login: ({ commit }) => {
+    throw new Error('login action should be implemented')
+  },
+  
+  fetchLists: ({ commit }) => {
+    throw new Error('addTask action should be implemented')
+  },
+
+  updateTask: ({ commit }) => {
+    throw new Error('updateTask action should be implemented')
+  },
+
+  removeTask: ({ commit }) => {
+    throw new Error('removeTask action should be implemented')
+  },
+
+  logout: ({ commit }) => {
+    throw new Error('logout action should be implemented')
+  }
+}
---
+++ src/store/mutations.js
@@ -0,0 +1,23 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.AUTH_LOGIN] (state, payload) {
+    throw new Error('AUTH_LOGIN mutation should be implemented')
+  },
+
+  [types.FETCH_ALL_TASKLIST] (state, payload) {
+    throw new Error('FETCH_ALL_TASKLIST mutatation should be implemented')
+  },
+  
+  [types.UPDATE_TASK] (state, payload) {
+    throw new Error('UPDATE_TASK mutation should be implemented')
+  },
+
+  [types.REMOVE_TASK] (state, payload) {
+    throw new Error('REMOVE_TASK mutation should be implement')
+  },
+
+  [types.AUTH_LOGOUT] (state) {
+    throw new Error('AUTH_LOGOUT mutation should be implement')
+  }
+}
