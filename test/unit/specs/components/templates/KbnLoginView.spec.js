91c607b57c3400f1d22603f054282109dfcd3e7a
Tatsuya Ichikawa <>
2018-12-20 22:42:40 +0900

    [Add] Add test code for login view

---
+++ test/unit/specs/components/templates/KbnLoginView.spec.js
@@ -0,0 +1,89 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import KbnLoginView from '@/components/templates/KbnLoginView.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('KbnLoginView', () => {
+  let actions
+  let $router
+  let store
+  let LoginFormComponentStub
+  
+  const triggerLogin = (loginView, target) => {
+    const loginForm = loginView.find(target)
+    loginForm.vm.onlogin('foo@domain.com', '12345678')
+    beforeEach(() => {
+      LoginFormComponentStub = {
+        name: 'KbnLoginForm',
+        props: ['onlogin'],
+        render: h => h('p', ['login form'])
+      }
+
+      $router = {
+        push: sinon.spy()
+      }
+
+      actions = {
+        login: sinon.stub()
+      }
+      store = new Vuex.Store({
+        state: {},
+        actions
+      })
+    })
+
+    describe('ログイン', () => {
+      let loginView
+      describe('成功', () => {
+        beforeEach(() => {
+          loginView = mount(KbnLoginView, {
+            mocks: { $router },
+            stubs: {
+              'kbn-login-form': LoginFormComponetStub
+            },
+            store,
+            localVue
+          })
+        })
+        it('ボードページのルートにリダイレクトすること', done => {
+          actions.login.resolves()
+          triggerLogin(loginView, LoginFormComponentStub)
+          loginView.vm.$nextTick(() => {
+            expect($router.push.called).to.equal(true)
+            expect($router.push.args[0][0].path).to.equal('/')
+            done()
+          })
+        })
+      })
+      describe('失敗', () => {
+        beforeEach(() => {
+          loginView = mount(KbnLoginView, {
+            stubs: {
+              'kbn-login-form': LoginFormComponentStub
+            },
+            store,
+            localVue
+          })
+          sinon.spy(loginView.vm, 'throwReject')
+        })
+        afterEach(() => {
+          loginView.vm.throwReject.restore()
+        })
+        it('エラー処理が呼び出されること', done => {
+          const messaege = 'login failed'
+          actions.login.rejects(new Error(message))
+          triggerLogin(loginView, LoginFormComponentStub)
+          loginView.vm.$nextTick(() => {
+            const callInfo = loginView.vm.throwReject
+            expect(callInfo.called).to.equal(true)
+            expect(callInfo.args[0][0].message).to.equal(message)
+            done()
+          })
+        })
+      })
+    })
+  }
+})
