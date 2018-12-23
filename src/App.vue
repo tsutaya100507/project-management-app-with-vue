ae3784ec99b29dbf5a524f0ac6301a1f530eaf58
Tatsuya Ichikawa <>
2018-12-24 01:38:45 +0900

    [Add] Add error handling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <div id="app">
+      <router-view/>
+    </div>
+    <ErrorBoundary id="app">
+      <router-view/>
+    </ErrorBoundary>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ErrorBoundary from './ErrorBoundary.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production'
+Vue.config.performance = true
+Vue.component(ErrorBoundary.name, ErrorBoundary)
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm:', vm)
+  console.error('errorHandler info:', info)
+}
 
 /* eslint-disable no-new */
 new Vue({
